5bc3db2b407e5ebce5672909e0d813a959b7d735
k-nakamura1984 <wanonaka76@gmail.com>
2021-04-10 16:12:26 +0900

    レビュー1回目の修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,53 +8,65 @@ OptionParser.new do |option_parser|
   option_parser.parse!(ARGV)
 end
 
-if ARGV.size.zero?
-  input = readlines.join
-  input_lines = input.lines.size
-  input_words = input.split(/\s+/).size
-  input_byte_size = input.to_s.bytesize
-  if option[:l]
-    print input_lines.to_s.rjust(8).to_s
+def show_status_loption
+  if ARGV.size.zero?
+    input_lines = readlines.join.lines.size
+    puts input_lines.to_s.rjust(8)
   else
-    print "#{input_lines.to_s.rjust(8)} #{input_words.to_s.rjust(7)}"
-    print input_byte_size.to_s.rjust(8).to_s
+    ARGV.each do |file_path|
+      file_lines = File.read(file_path).lines.size
+      file_name = File.basename(file_path)
+      puts "#{file_lines.to_s.rjust(8)} #{file_name}"
+    end
   end
-  puts
 end
 
-total_file_lines = 0
-total_file_words = 0
-total_file_byte_size = 0
+def show_status
+  if ARGV.size.zero?
+    input = readlines.join
+    input_lines = input.lines.size
+    input_words = input.split(/\s+/).size
+    input_byte_size = input.bytesize
+    print "#{input_lines.to_s.rjust(8)} #{input_words.to_s.rjust(7)}"
+    puts input_byte_size.to_s.rjust(8)
+  else
+    ARGV.each do |file_path|
+      file = File.read(file_path)
+      file_lines = file.lines.size
+      file_words = file.split(/\s+/).size
+      file_byte_size = file.bytesize
+      file_name = File.basename(file_path)
+      print "#{file_lines.to_s.rjust(8)} #{file_words.to_s.rjust(7)}"
+      puts "#{file_byte_size.to_s.rjust(8)} #{file_name} "
+    end
+  end
+end
+
+def count_total
+  $total_file_lines = 0
+  $total_file_words = 0
+  $total_file_byte_size = 0
+  ARGV.each do |file_path|
+    file = File.read(file_path)
+    $total_file_lines += file.lines.size
+    $total_file_words += file.split(/\s+/).size
+    $total_file_byte_size += file.bytesize
+  end
+end
+
 if option[:l]
-  ARGV.each_index do |index|
-    file = File.read(ARGV[index])
-    file_lines = file.lines.size
-    total_file_lines += file.lines.size
-    file_name = File.basename(ARGV[index])
-    print "#{file_lines.to_s.rjust(8)} #{file_name}"
-    puts
-  end
-  if ARGV.size > 1
-    print "#{total_file_lines.to_s.rjust(8)} total"
-    puts
-  end
+  show_status_loption
 else
-  ARGV.each_index do |index|
-    file = File.read(ARGV[index])
-    file_lines = file.lines.size
-    total_file_lines += file.lines.size
-    file_words = file.split(/\s+/).size
-    total_file_words += file.split(/\s+/).size
-    file_byte_size = file.to_s.bytesize
-    total_file_byte_size += file.to_s.bytesize
-    file_name = File.basename(ARGV[index])
-    print "#{file_lines.to_s.rjust(8)} #{file_words.to_s.rjust(7)}"
-    print "#{file_byte_size.to_s.rjust(8)} #{file_name} "
-    puts
-  end
-  if ARGV.size > 1
-    print "#{total_file_lines.to_s.rjust(8)} #{total_file_words.to_s.rjust(7)}"
-    print "#{total_file_byte_size.to_s.rjust(8)} total"
-    puts
+  show_status
+end
+
+if ARGV.size > 1
+  if option[:l]
+    count_total
+    puts "#{$total_file_lines.to_s.rjust(8)} total"
+  else
+    count_total
+    print "#{$total_file_lines.to_s.rjust(8)} #{$total_file_words.to_s.rjust(7)}"
+    puts "#{$total_file_byte_size.to_s.rjust(8)} total\n"
   end
 end
