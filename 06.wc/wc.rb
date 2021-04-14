cac27d99263afe1b997d81d3fa0e5630b0b9646f
k-nakamura1984 <wanonaka76@gmail.com>
2021-04-14 20:41:54 +0900

    totalの表示をshowメソッドに組み込み

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -9,62 +9,45 @@ OptionParser.new do |option_parser|
 end
 
 def show_status_loption
+  width = 8
   if ARGV.size.zero?
-    input_lines = readlines.join.lines.size
-    puts input_lines.to_s.rjust(8)
+    input = readlines.join
+    puts input.lines.size.to_s.rjust(width)
   else
     ARGV.each do |file_path|
       file_lines = File.read(file_path).lines.size
-      file_name = File.basename(file_path)
-      puts "#{file_lines.to_s.rjust(8)} #{file_name}"
+      print file_lines.to_s.rjust(width)
+      puts " #{File.basename(file_path)}"
     end
+    puts "#{ARGV.sum { |file_path| File.read(file_path).lines.size }.to_s.rjust(width)} total" if ARGV.size > 1
   end
 end
 
 def show_status
+  width = 8
   if ARGV.size.zero?
     input = readlines.join
-    input_lines = input.lines.size
-    input_words = input.split(/\s+/).size
-    input_byte_size = input.bytesize
-    print "#{input_lines.to_s.rjust(8)} #{input_words.to_s.rjust(7)}"
-    puts input_byte_size.to_s.rjust(8)
+    print input.lines.size.to_s.rjust(width)
+    print input.split(/\s+/).size.to_s.rjust(width)
+    puts input.bytesize.to_s.rjust(width)
   else
     ARGV.each do |file_path|
       file = File.read(file_path)
-      file_lines = file.lines.size
-      file_words = file.split(/\s+/).size
-      file_byte_size = file.bytesize
-      file_name = File.basename(file_path)
-      print "#{file_lines.to_s.rjust(8)} #{file_words.to_s.rjust(7)}"
-      puts "#{file_byte_size.to_s.rjust(8)} #{file_name} "
+      print file.lines.size.to_s.rjust(width)
+      print file.split(/\s+/).size.to_s.rjust(width)
+      print file.bytesize.to_s.rjust(width)
+      puts  " #{File.basename(file_path)} "
+    end
+    if ARGV.size > 1
+      print ARGV.sum { |file_path| File.read(file_path).lines.size }.to_s.rjust(width)
+      print ARGV.sum { |file_path| File.read(file_path).split(/\s+/).size }.to_s.rjust(width)
+      puts "#{ARGV.sum { |file_path| File.read(file_path).bytesize }.to_s.rjust(width)} total"
     end
   end
-end
-
-def count_total_file_lines
-  ARGV.sum { |file_path|  File.read(file_path).lines.size }
-end
-
-def count_total_file_words
-  ARGV.sum { |file_path|  File.read(file_path).split(/\s+/).size }
-end
-
-def count_total_file_bytesize
-  ARGV.sum { |file_path|  File.read(file_path).bytesize }
 end
 
 if option[:l]
   show_status_loption
 else
   show_status
-end
-
-if ARGV.size > 1
-  if option[:l]
-    puts "#{count_total_file_lines.to_s.rjust(8)} total"
-  else
-    print "#{count_total_file_lines.to_s.rjust(8)} #{count_total_file_words.to_s.rjust(7)}"
-    puts "#{count_total_file_bytesize.to_s.rjust(8)} total"
-  end
 end
